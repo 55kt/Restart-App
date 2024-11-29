b8f4a683dc9b98790290722da61885e514db4bed
Vlad <160039489+55kt@users.noreply.github.com>
2024-11-29 18:47:41 +0200

    First commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Vlad on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
