632bcd6f90307e3609020ab0e682d1c2929d80ac
Vlad <160039489+55kt@users.noreply.github.com>
2024-11-29 20:21:00 +0200

    Onboarding Screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,16 +13,109 @@ struct OnboardingView: View {
     
     // MARK: - Body
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        } //: Vstack
+            VStack(spacing: 20) {
+                // MARK: - Header
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                    
+                    Text("""
+Its not how much we give but
+huw much love we put into giving.
+""")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: Header
+                
+                // MARK: - Center
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        
+                        Image("character-1")
+                            .resizable()
+                            .scaledToFit()
+                    } //: Zstack
+                } //: Center
+                
+                Spacer()
+                
+                // MARK: - Footer
+                
+                ZStack {
+                    // Parts of the custom button
+                    
+                    // 1. Background (Static)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. Call-to-action (Static)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. Capsule (Dinamic width)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. Circle (Draggable)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundStyle(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    } //: Hstack
+                    
+                } //: Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
+            } //: Vstack
+        } //: Zstack
     }
 }
 
