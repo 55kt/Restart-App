88eed0e86d7bd728eceb1ad0cbe54b544cf09cd7
Vlad <160039489+55kt@users.noreply.github.com>
2024-11-30 17:13:35 +0200

    Home view
    Circle Component & Properties

---
+++ Restart/Views/CircleGroupView.swift
@@ -0,0 +1,36 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Vlad on 30/11/24.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    // MARK: - Properties
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: Zstack
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    ZStack {
+        Color("ColorBlue")
+            .ignoresSafeArea(.all, edges: .all)
+        
+        CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+    }
+}
